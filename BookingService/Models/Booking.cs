3cc900cc668315d033e6c1cd0ab916e2059b7a1e
christofferPerch <cph-cn332@cphbusiness.dk>
2024-10-04 09:56:48 +0200

    Booking

---
+++ BookingService/Models/Booking.cs
@@ -0,0 +1,18 @@
+﻿namespace BookingService.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int TrailerId { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public bool IsInsured { get; set; }
+        public bool IsOverdue { get; set; }
+        public decimal LateFee { get; set; }
+        public bool IsOverNight { get; set; }
+        public decimal OverNightFee { get; set; }
+        public DateTime? ActualReturnTime { get; set; }
+
+    }
+}
